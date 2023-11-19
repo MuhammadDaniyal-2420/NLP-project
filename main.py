6a9538ffbac7793acbc675bdbb59877d7b641adc
MuhammadDaniyal-2420 <126562760+MuhammadDaniyal-2420@users.noreply.github.com>
2023-11-19 20:35:45 +0500

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,9 @@ data = pd.read_csv('consolidated_coin_data.csv')
 # Data preprocessing steps
 data['Date'] = pd.to_datetime(data['Date'])
 
+# Replace commas in numeric columns and convert to float
 numeric_columns = ['Open', 'High', 'Low', 'Close', 'Volume', 'Market Cap']
+
 for col in numeric_columns:
     data[col] = data[col].str.replace(',', '').astype(float)
 
@@ -26,78 +28,94 @@ st.markdown("---")
 st.sidebar.title('Enter Data for Prediction')
 
 # Currency name dropdown
-currency_names = [
-    'tezos', 'binance-coin', 'eos', 'bitcoin', 'tether',
-    'xrp', 'bitcoin-cash', 'stellar', 'litecoin', 'ethereum',
-    'cardano', 'bitcoin-sv'
-]
-
+currency_names = data['Currency'].unique()
 selected_currency = st.sidebar.selectbox('Select Currency Name', currency_names)
 
 if selected_currency:
     filtered_data = data[data['Currency'] == selected_currency]
 
-    open_prices = [1.28, 0.909666, 44.53, 90.17, 238.02, 8320.83, None]
-    high_prices = [1.32, 0.965669, 7.09, 24.58, 8410.71, 395.5, None]
-    low_prices = [367.83, 4377.46, 23.16, 3.79, 0.606857, None]
-    close_prices = [0.850526, 1.23, 5285.14, 382.3, None]
-    volumes = [0, 3284328, 792592, 62818.3, None]
-    market_caps = [951600, 1451600, 9048082, None]
+    # Display the first two lines of data for the selected currency
+    st.subheader('First Two Lines of Data:')
+    st.table(filtered_data.head(2))
 
-    col1, col2 = st.sidebar.columns(2)
+    # Model selection
+    st.sidebar.subheader('Select Model:')
+    model_options = ['Random Forest', 'Gradient Boosting', 'Support Vector Machine']
+    selected_model = st.sidebar.selectbox('Choose a Model', model_options)
 
-    with col1:
-        open_price = st.selectbox('Enter Open Price', open_prices)
-        high_price = st.selectbox('Enter High Price', high_prices)
-        low_price = st.selectbox('Enter Low Price', low_prices)
-        close_price = st.selectbox('Enter Close Price', close_prices)
+    if selected_model == 'Random Forest':
+        model = RandomForestRegressor(n_estimators=100, random_state=42)
+    elif selected_model == 'Gradient Boosting':
+        model = GradientBoostingRegressor(n_estimators=100, random_state=42)
+    elif selected_model == 'Support Vector Machine':
+        model = SVR()
 
-    with col2:
-        volume = st.selectbox('Enter Volume', volumes)
-        market_cap = st.selectbox('Enter Market Cap', market_caps)
+    # User input for historical prices
+    st.sidebar.subheader('Enter Custom Historical Prices:')
+    open_price = st.sidebar.number_input('Open Price', min_value=0.0)
+    high_price = st.sidebar.number_input('High Price', min_value=0.0)
+    low_price = st.sidebar.number_input('Low Price', min_value=0.0)
+    close_price = st.sidebar.number_input('Close Price', min_value=0.0)
+    volume = st.sidebar.number_input('Volume', min_value=0)
+    market_cap = st.sidebar.number_input('Market Cap', min_value=0)
 
-    if None in [open_price, high_price, low_price, close_price, volume, market_cap]:
-        st.warning('Please enter custom values')
-    else:
-        selected_features = ['Open', 'High', 'Low', 'Close', 'Volume', 'Market Cap']
-        X = filtered_data[selected_features]
-        y = filtered_data['Close']  # Assuming 'Close' price is the target variable
+    # User input for model selection
+    st.sidebar.subheader('Model Parameters:')
+    # Add parameters for the selected model if needed
 
+    user_input = pd.DataFrame({
+        'Open': [open_price],
+        'High': [high_price],
+        'Low': [low_price],
+        'Close': [close_price],
+        'Volume': [volume],
+        'Market Cap': [market_cap]
+    })
+
+    if st.sidebar.button('Predict'):
         # Split data into training and testing sets
+        X = filtered_data[['Open', 'High', 'Low', 'Close', 'Volume', 'Market Cap']]
+        y = filtered_data['Close']
+
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        # Model selection dropdown
-        models = {
-            'Random Forest': RandomForestRegressor(n_estimators=100, random_state=42),
-            'Gradient Boosting': GradientBoostingRegressor(n_estimators=100, random_state=42),
-            'Support Vector Machine': SVR(kernel='rbf')
-        }
-        selected_model = st.sidebar.selectbox('Select Model', list(models.keys()))
-
-        # Train the selected model
-        model = models[selected_model]
+        # Train the model
         model.fit(X_train, y_train)
 
         # Make predictions
-        y_pred = model.predict(X_test)
+        prediction = model.predict(user_input)
+
+        # Display prediction to the user
+        st.markdown('---')
+        st.success('Predicted Close Price:')
+        st.title(f"${prediction[0]:,.2f}")
+
+        # Visualize predictions vs. actual values
+        st.subheader('Predictions vs. Actual Values:')
+        plt.figure(figsize=(10, 5))
+        plt.plot(filtered_data['Date'], filtered_data['Close'], label='Actual Close Price', color='blue')
+        plt.scatter(filtered_data['Date'].iloc[-1], prediction[0], label='Predicted Close Price', color='red')
+        plt.title('Predicted vs. Actual Close Price Over Time')
+        plt.xlabel('Date')
+        plt.ylabel('Close Price')
+        plt.legend()
+        st.pyplot(plt)
 
         # Model evaluation
         st.subheader("Model Performance Metrics")
-        st.write(f"Mean Absolute Error: {mean_absolute_error(y_test, y_pred):.2f}")
-        st.write(f"Mean Squared Error: {mean_squared_error(y_test, y_pred):.2f}")
-        st.write(f"R-squared: {r2_score(y_test, y_pred):.2f}")
+        st.write(f"Mean Absolute Error: {mean_absolute_error(y_test, model.predict(X_test)):.2f}")
+        st.write(f"Mean Squared Error: {mean_squared_error(y_test, model.predict(X_test)):.2f}")
+        st.write(f"R-squared: {r2_score(y_test, model.predict(X_test)):.2f}")
 
-        # Visualize predictions vs actual values
-        plt.figure(figsize=(10, 6))
-        sns.scatterplot(x=y_test.index, y=y_test, label='Actual Values')
-        sns.lineplot(x=y_test.index, y=y_pred, label='Predicted Values', color='orange')
-        plt.title('Actual vs Predicted Values')
-        plt.xlabel('Data Index')
-        plt.ylabel('Close Price')
-        st.pyplot(plt)
+    # Plot historical prices
+    st.subheader('Historical Prices:')
+    plt.figure(figsize=(10, 5))
+    plt.plot(filtered_data['Date'], filtered_data['Close'], label='Close Price', color='green')
+    plt.title('Historical Close Price Over Time')
+    plt.xlabel('Date')
+    plt.ylabel('Close Price')
+    plt.legend()
+    st.pyplot(plt)
 
-        # Display predicted value to the user
-        st.markdown('---')
-        st.success(f'Predicted Close Price: {y_pred[0]:.2f}')
 else:
     st.warning('Please select a Currency Name')
